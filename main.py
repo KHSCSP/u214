43c111ca03903c4ca13f28a481d31024b0578c74
scneeman <30477959+scneeman@users.noreply.github.com>
2024-03-07 13:26:54 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+# define your function
+
+
+# main window
+root = tk.Tk()
+root.wm_geometry("300x200")
+
+# create a frame for logging in
+
+
+# create label and textbox for username
+
+
+
+# another label and textbox for password
+
+
+# a button to login
+
+
+# another frame for frame_auth
+
+
+
+
+root.mainloop()
